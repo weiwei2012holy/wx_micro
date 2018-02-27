927f4b903fb5fe8e78bad34e054475c252ad1737
yuwei <463313681@qq.com>
2018-02-27 09:26:32 +0800

    test

---
+++ resources/views/hr.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>hr</title>
+</head>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/swiper.min.css">
+<body>
+
+	<div class="swiper-container" >
+		<div class="swiper-wrapper">
+			<div class="swiper-slide main">
+				<div class="top-image-box">
+					<img src="img/bg-header.png" class="bg-header animated pulse">
+					<img src="img/want-you.png" class="want-you animated rotateInDownLeft">
+					<img src="img/join-us.png" class="join-us animated rotateInUpRight">
+					<img src="img/index-girls.png" alt="" class='index-girls animated fadeInUp'>
+					<img src="img/mk.png" alt="" class='index-mk animated-3s flipInX'>
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<div class='has-border'>
+					<img src="img/top-girls.png" style='width: 60%;left: 20%;'>
+					<h2 class='second-left-text'>想拥有一个工资突破如火箭的方法吗？</h2>
+					
+					<img src="img/we-have.png" alt="" style='width: 80%;'>
+					<img src="img/roket.png" style='width: 50px;float: left;margin-left: 30px;margin-top: 10px;'>
+					<div class='second-footer-box'>
+						<div class='second-right-text'>无需你多才多艺</div>
+						<div class='second-right-text'>只要你热爱销售行业</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="swiper-slide trird-box" style='background: #FED732'>
+				<div class='has-border'>
+					<img src="img/what-we-need.png" style='width: 60%;left: 20%;margin-top: 10%;'>
+					<div class='job-name'>导购</div>
+					<p>平均工资4000-5000<small>(只限女同胞)</small></p>
+					<div class='job-name'>店助</div>
+					<p>平均工资5000-6000<small>(只限女同胞)</small></p>
+					<div class='job-name'>店长</div>
+					<p>平均工资7000-8000<small>(只限女同胞)</small></p>
+					<div class='job-name'>督导</div>
+					<p>平均工资8000-12000<small>(只限女同胞)</small></p>
+				</div>
+			</div>
+				<div class="swiper-slide">
+				<div class='has-border'>
+					<img src="img/fuli.png" style='width: 100%;margin-top: 5%'>
+				</div>
+			</div>
+						<div class="swiper-slide">
+				<div class='has-border'></div>
+			</div>
+
+
+		</div>
+	</div>
+	<div class="maka-Arrow" style="display: block;">
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAAkCAMAAADip6m2AAAAolBMVEUAAAAAAAAAAAAAAAAAAAABAQEAAAAAAAAHBwcAAAD////9/f37+/vt7e25ubn39/cKCgrz8/Pe3t7c3Nz+/v729vbW1tZra2tKSkoAAABpaWlgYGD9/f38/Pz5+fn5+fn39/fz8/Px8fHu7u7q6urm5ubj4+Pd3d3y8vLX19fJycnAwMC4uLisrKyhoaGVlZWGhoajo6MpKSkxMTGAgID///9oDYyzAAAANXRSTlMAAwcLFR0SGRAO+efJbSOcIqBDQvLNbTEpGxEQ9e3h29XDvLOsnJOJiIFpYVlNRkE7MiwfDLl52bwAAAGFSURBVEjH3ZXZboMwFERbHAMhC6EFQtrs+777/3+t98bGjmwqy32plHmLPYfMiYTy9nJ5F/kb693j+O4h7Y567SZjzbYHuDv72WCQxgfSjixpReyRqEWAdmK/YyaTfAHtpivjIo6TUReTZYxxcZzupju/XufP4g660aYD2USmuF13sKeP7Ae6uF13fKZBDRLQ81gTt+rOCmD9MPSBLmaauEV3hWidQOqIr57FLbrpDllCPAghSO9SU7xad3QCNiRQgwAeAn0a6eLVutOC4mRk+SCcToupIW7qsmWnByw25B3Svc6SaeKGbn9LA8UqGsW3fVMc2VJ3eJS6apcUPw5LcVmAq1J30qWBL1mN9gPanZTiUBE3pe5C6cpo4gtRjXkJjtf8cbnSlTHEcz5yTTwBh00UOVToVogfGvibhQq+pCyr0q0Wz1h6kTDsyZObPvn36bckh6qA8YE1eIUEa6GxyyeKEzhC1MLy6byL3yNP4BNn7TSWVRffHYHaI7qq7PB3qMr/mh8s20b2bOZlXQAAAABJRU5ErkJggg==" alt="">
+  </div>
+</body>
+<script src="js/swiper.min.js"></script>
+	<script>
+		var swiper = new Swiper('.swiper-container', {
+			direction: 'vertical',
+			slidesPerView: 1,
+			spaceBetween: 30,
+			mousewheel: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		});
+	</script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/hr', function () {
+    return view('hr');
+});
